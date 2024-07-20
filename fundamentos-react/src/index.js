c38426e9a6a524675079c8a82c17775c97fc1057
estrela801 <rohenri0808@gmail.com>
2024-07-20 16:14:36 -0300

    feat: adicionando exercícios à um componente de card

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -6,12 +6,26 @@ import Segundo from './componentes/basicos/segundo'
 import A from './componentes/medios/MediosInterno/teste'
 import B from './componentes/medios/lowercase'
 import Aleatorio from './componentes/basicos/Aleatorio'
+import Card from './componentes/layout/Card'
+
 ReactDOM.render(
     <div  id="id-principal">
-        <Primeiro></Primeiro>
+
+        <Card titulo='Primeiro Ex'>
+
+            {<Primeiro></Primeiro>}
+        </Card>
+        <Card titulo='Segundo Ex'>
         <Segundo testedepropriedadedentrodoreact= 'teste de porpriedades através dos import e export(deu certo mesmo)'></Segundo>
+        </Card>
+        <Card titulo='Teste de renderização'>
         <A></A>
+        </Card>
+        <Card titulo='Teste de Case'>
         <B></B>
+        </Card>
+        <Card titulo='Número aleatório'>
         <Aleatorio min={1} max={10}></Aleatorio>
+        </Card>
     </div>
     , document.querySelector('#root'))
