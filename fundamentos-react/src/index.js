82a646b18d9b750c64a448f3aeaa326752ce94d9
estrela801 <rohenri0808@gmail.com>
2024-07-20 15:46:56 -0300

    feat: adicionando novo componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -5,11 +5,13 @@ import Primeiro from './componentes/basicos/Primeiro'
 import Segundo from './componentes/basicos/segundo'
 import A from './componentes/medios/MediosInterno/teste'
 import B from './componentes/medios/lowercase'
+import Aleatorio from './componentes/basicos/Aleatorio'
 ReactDOM.render(
     <div  id="id-principal">
         <Primeiro></Primeiro>
         <Segundo testedepropriedadedentrodoreact= 'teste de porpriedades através dos import e export(deu certo mesmo)'></Segundo>
         <A></A>
         <B></B>
+        <Aleatorio min={1} max={10}></Aleatorio>
     </div>
     , document.querySelector('#root'))
