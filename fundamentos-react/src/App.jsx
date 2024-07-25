8b32c1861743bc2cf1e345674d2429e85e36ea15
estrela801 <rohenri0808@gmail.com>
2024-07-25 16:25:37 -0300

    feat: adicionando novo exercício e identando

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -8,29 +8,32 @@ import A from "./componentes/medios/MediosInterno/teste";
 import B from "./componentes/medios/lowercase";
 import Aleatorio from "./componentes/basicos/Aleatorio";
 import Card from "./componentes/layout/Card";
+import Familia from "./componentes/basicos/Familia";
 
-export default function App (){
+export default function App() {
   ReactDOM.render(
     <div id="id-principal">
-    <div className="exs">
-      <Card titulo="Primeiro Ex">{<Primeiro></Primeiro>}</Card>
-      <Card titulo="Segundo Ex">
-        <Segundo testedepropriedadedentrodoreact="teste de porpriedades através dos import e export(deu certo mesmo)"></Segundo>
-      </Card>
-      <Card titulo="Teste de renderização">
-        <A></A>
-      </Card>
-      <Card titulo="Teste de Case">
-        <B></B>
-      </Card>
-      <Card titulo="Número aleatório">
-        <Aleatorio min={1} max={10}></Aleatorio>
-      </Card>
-    </div>
-  </div>,
-  document.querySelector("#root")
-  )
-  
-}
+      <div className="exs">
+        <Card titulo="Primeiro Ex">{<Primeiro></Primeiro>}</Card>
 
- 
+        <Card titulo="Segundo Ex" cor="">
+          <Segundo testedepropriedadedentrodoreact="teste de porpriedades através dos import e export(deu certo mesmo)"></Segundo>
+        </Card>
+
+        <Card titulo="Teste de renderização">
+          <A></A>
+        </Card>
+        <Card titulo="Teste de Case">
+          <B></B>
+        </Card>
+        <Card titulo="Número aleatório">
+          <Aleatorio min={1} max={10}></Aleatorio>
+        </Card>
+        <Card titulo='Exercicio familia'>
+          <Familia></Familia>
+        </Card>
+      </div>
+    </div>,
+    document.querySelector("#root")
+  );
+}
