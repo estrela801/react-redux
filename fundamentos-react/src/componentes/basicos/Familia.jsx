69063df86549cb47217f9fdb11bd03b6cb4504df
estrela801 <rohenri0808@gmail.com>
2024-07-25 16:26:08 -0300

    feat: criando componentes de novos exercícios

---
+++ fundamentos-react/src/componentes/basicos/Familia.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FamiliaMembros from "./FamiliaMembros";
+
+export default proprs =>{
+    return(
+        <div>
+            <FamiliaMembros nome='Rodrigo' sobrenome='Henrique'></FamiliaMembros>
+            <FamiliaMembros nome='Celso' sobrenome='Henrique'></FamiliaMembros>
+            <FamiliaMembros nome='Danilo' sobrenome='Henrique'></FamiliaMembros>
+            <FamiliaMembros nome='Felipe' sobrenome='Henrique'></FamiliaMembros>
+            
+        </div>
+    )
+}
---
+++ fundamentos-react/src/componentes/basicos/FamiliaMembros.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props =>{
+    return(
+        <div>{props.nome} <strong>{props.sobrenome}</strong></div>
+    )
+}
